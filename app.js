c9ad084ab55d8cce6211c21f9c1a8bfcc7a29bdd
SamuelLnds <samuel.landais.pro@gmail.com>
2025-05-16 15:43:47 +0200

    fix liens toc

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ class TOCGenerator {
       if (!h3.id) h3.id = TOCGenerator.slugify(h3.textContent);
       const li = document.createElement('li');
       const a  = document.createElement('a');
-      a.setAttribute('href', `./#${h3.id}`);
+      a.href = `ressources/#${h3.id}`;
       a.textContent = h3.textContent.replace(/<\/?code>/g, '');
       li.appendChild(a);
       this.tocList.appendChild(li);
