753019377d73ed1f98193f3d1fdb38d739acda8b
SamuelLnds <samuel.landais.pro@gmail.com>
2025-05-16 15:25:41 +0200

    update du footer link vers ressources

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ class MyFooter extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `
         <footer style="text-align:center; padding:1rem; border-top:1px solid var(--border); color:var(--text)">
-          <a href="ressources.html" style="color: var(--accent); text-decoration: none;">📚 Voir les ressources SQL</a>
+          <a href="./ressources/" style="color: var(--accent); text-decoration: none;">📚 Voir les ressources SQL</a>
         </footer>
       `;
     }
